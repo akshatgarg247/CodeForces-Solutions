5ee6a632d6f954214d03b0a1990a717e99860d56
Akshat Garg <akshatgargdli@gmail.com>
2026-04-15 14:31:46 +0530

    Create 1807A.cpp

---
+++ 1807A.cpp
@@ -0,0 +1,20 @@
+// Author @25bce10693
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+  int n;
+  cin >> n;
+  while (n--) {
+    int a, b, c; cin >> a >> b >> c;
+    if (a+b == c) {
+      cout << "+\n";
+    } else {
+      cout << "-\n";
+    }
+  }
+    return 0;
+}
