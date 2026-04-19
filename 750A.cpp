ee2404a2a64e0af0499623fa52090de1b2000708
Akshat Garg <akshatgargdli@gmail.com>
2026-04-18 23:26:51 -0700

    Create 750A.cpp

---
+++ 750A.cpp
@@ -0,0 +1,19 @@
+// Author @2025BCE10693
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k; cin >> n >> k;
+    int travel_time = 240;
+    int time = 0;
+    int count = 0;
+    for (int i=1; i<=n; i++) {
+      time = time + 5*i;
+      if (time + k - travel_time <= 0) {
+        count++;
+      } 
+    }
+    cout << count;
+    return 0;
+}
